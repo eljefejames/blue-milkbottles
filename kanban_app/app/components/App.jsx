fed8b3bccbc828d84e28d98071e8f93085a312d6
eljefejames <lukejamesmail@gmail.com>
2015-07-28 10:48:04 +0700

    Commit before adding lanes

--- kanban_app/app/components/App.jsx
+++ kanban_app/app/components/App.jsx
@@ -1,73 +1,47 @@
+import AltContainer from 'alt/AltContainer';
 import React from 'react';
 import Notes from './Notes';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import persist from '../decorators/persist';
 import storage from '../libs/storage';
-import connect from '../decorators/connect';
 
 const noteStorageName = 'notes';
 
 @persist(storage, noteStorageName, () => NoteStore.getState())
-@connect(NoteStore)
-
-
 export default class App extends React.Component {
   constructor(props) {
-    notes: Array;
-  }) {
-   super(props);
+    super(props);
 
-this.addItem = this.addItem.bind(this);
-this.itemEdited = this.itemEdited.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.itemEdited = this.itemEdited.bind(this);
 
-this.storeChanged = this.storeChanged.bind(this);
-
-NoteActions.init(storage.get(noteStorageName));
-this.state = NoteStore.getState();
-}
-/*
-
-The addition of the Connect decorators allows to remove to the mounting bits
-componentDidMount() {
-   NoteStore.listen(this.storeChanged);
- }
- componentWillUnmount() {
-   NoteStore.unlisten(this.storeChanged);
- }
- storeChanged(state) {
-   this.setState(NoteStore.getState());
- }
- */
-
+    NoteActions.init(storage.get(noteStorageName));
+  }
   render() {
-    var notes = this.state.notes;
-
     return (
       <div>
-       <button onClick={this.addItem}>+</button>
-       <Notes items={notes} onEdit={this.itemEdited} />
-     </div>
+        <button onClick={this.addItem}>+</button>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={ {
+            items: () => NoteStore.getState().notes || []
+          } }
+        >
+          <Notes onEdit={this.itemEdited} />
+        </AltContainer>
+      </div>
     );
   }
-
-
-addItem() {
-  NoteActions.create('New Task');
-}
-
-itemEdited(id, task) {
-  if (task) {
-    NoteActions.update({id, task});
+  addItem() {
+    NoteActions.create('New task');
   }
-  else {
-    NoteActions.remove(id);
+  itemEdited(id, task) {
+    if(task) {
+      NoteActions.update({id, task});
+    }
+    else {
+      NoteActions.remove(id);
+    }
   }
 }
-}
-export default persist(
-  connect(App, NoteStore),
-  storage,
-  noteStorageName,
-  () => NoteStore.getState()
-);
